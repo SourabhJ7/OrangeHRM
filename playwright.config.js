8cb73e444e27cedf628820056d0bee5ede14c226
SourabhJ7 <80623989+SourabhJ7@users.noreply.github.com>
2026-08-30 17:12:54 +0530

    Update playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,35 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+    testDir: './tests',
+
+    timeout: 30000,
+
+    use: {
+        headless: true,
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+        trace: 'retain-on-failure'
+    },
+
+    projects: [
+        {
+            name: 'chromium',
+            use: {
+                browserName : 'chromium'
+            }
+        },
+        {
+            name: 'webkit',
+            use: {
+                browserName : 'webkit'
+            }
+        },
+        {
+            name: 'firefox',
+            use: {
+                browserName : 'firefox'
+            }
+        }
+    ]
+});
